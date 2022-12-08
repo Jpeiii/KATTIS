3adab2973308610d94dd0faf3040fae4d95825e2
Jin Pei Ng <jin.pei.ng95@gmail.com>
2022-12-08 18:24:52 +0800

    added cpp solution for average character

--- Average_Character/Average_Character.cpp
+++ Average_Character/Average_Character.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <cmath>
 
 using namespace std;
 
@@ -7,10 +8,22 @@ int main()
 {
     int sum = 0;
     string s;
-    cin >> s;
-    for (int i = 0; i < s.size(); i++)
+    /*
+    why use getline instead of cin
+    The cin function can be used to read input from the standard input, but it has some limitations.
+    It is designed to read input values separated by white space (spaces, tabs, newlines, etc.),
+    and it stops reading when it encounters a white space character.
+    This means that it cannot read input strings that contain spaces or other special characters.
+    For example, if you try to read the input string "hello world" using cin, it will only read the first word "hello",
+    and the rest of the input string "world" will be ignored.
+    This is why the code uses the getline function instead, which can read an entire line of input,
+    including spaces and other special characters.
+    */
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++)
     {
-        sum += int(s[i]);
+        sum += (int)s[i];
     }
-    cout << char((sum / s.size()));
+    cout << char((sum / s.length()));
+    return 0;
 }
